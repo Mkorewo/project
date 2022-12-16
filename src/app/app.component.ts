49a3334ddc17a2bedbef8a776756b8a829a18425
Mkorewo <korewo.marcel@gmail.com>
2022-12-16 12:00:14 +0100

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'project';
-  obrazek:string = "obrazek.png";
+  content:string = `
+  <p>a</p> <!--akapit-->
+    <a href='#'>odnośnik</a> <!--odnośnik-->
+    <ol className='questions-list'>
+      <li>element</li>
+      <li>element</li>
+      <li>element</li>
+      <li>element</li>
+    </ol> <!--lista-->
+    <img src="obrazek.png" alt="obrazek"> <!--obrazek-->
+  `;
 }
